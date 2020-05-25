7cde21729c78b20cf1d69ff16784e4510ba700e3
Alejandro <alejandro.melgar38@gmail.com>
2020-05-24 21:38:43 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace RomanNumeralConverter
+{
+    public class Converter
+    {
+        public string IntToRoman(int number)
+        {
+            StringBuilder returnString = new StringBuilder();
+            string test = "";
+            string[] romanVals = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
+            int[] digits = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 }; //values of all possible outcomes including 6 subtraction values are stored in array
+            while (number > 0) //if number is 0 then you are done and can return final string
+            {
+                for (int i = digits.Count() - 1; i >= 0; i--)//start at the end of the digits array and descend to first entry
+                {
+                    if (number / digits[i] >= 1)//check to see if number will be smaller than value at digits[i] 
+                    {//if it is then subtract you can subtract value at digits[i] from number 
+                        /*Console.WriteLine("Before: {0}", number);*/
+                        number -= digits[i];
+                        /*Console.WriteLine("After: {0}", number);*/
+                        //returnString.Append(romanVals[i]);
+                        test = test + romanVals[i];
+                        //add string to final string
+                        break;
+                    }
+                }
+            }
+            //Console.WriteLine("This is the test string {0}",test);
+            return test;//returnString.ToString(); 
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "";
+            string leave = "";
+            Converter Roman = new Converter();
+            while (leave != "y")
+            {
+                Console.WriteLine("Please enter a numerical Value: ");
+                value = Console.ReadLine();
+
+                string test = Roman.IntToRoman(int.Parse(value));
+
+                Console.WriteLine("This is the equivalent of {0} in Roman Numerals: {1}", value, test);
+                Console.WriteLine("Do you wish to exit?(y/n): ");
+                leave = Console.ReadLine();
+            }
+            
+            /*Console.WriteLine("Hello World!");*/
+        }
+    }
+}
